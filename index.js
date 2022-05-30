b510ea33c9207c5f6bc38e3727e49e78e3f89de0
web-altaf-1 <web.altaf.1@gmail.com>
2022-05-30 23:41:09 +0600

    all added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const jwt = require('jsonwebtoken');
 require('dotenv').config();
+const port = process.env.PORT || 5000;
 const app = express();
-const port = process.env.PORT || 5000 ;
 
 app.use(cors());
 app.use(express.json());
@@ -14,32 +14,44 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
-    try {
-      await client.connect();
-      console.log('Database Connected');
-      const productCollection = client.db('db_manufacture').collection('parts');
+  try {
+    await client.connect();
 
-      app.get('/products', async (req, res) => {
-        const query = {};
-        const cursor = productCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services);
-      });
-      
-    } 
-    finally {
-      
-    }
+    console.log('Database Connected');
+
+    const productCollection = client.db('db_manufacture').collection('parts');
+
+    app.get('/products', async (req, res) => {
+      const query = {};
+      const cursor = productCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get('/products/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)};
+      const services = await productCollection.findOne(query);
+      res.send(services);
+    })
+
+    
   }
-  run().catch(console.dir);
+
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
 
 
 
-app.get('/',(req,res)=>{
-    res.send('Manufacturer website is running')
+app.get('/', (req, res) => {
+  res.send('Manufacturer website is running')
 })
 
-app.listen(port,()=>{
-    console.log('listening to the port',port);
+app.listen(port, () => {
+  console.log('listening to the port', port);
 })
 
