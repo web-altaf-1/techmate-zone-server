e6416485adc9f1ff8507477f2582b23d669bf29b
web-altaf-1 <web.altaf.1@gmail.com>
2022-05-31 22:54:22 +0600

    heroku app problem

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const jwt = require('jsonwebtoken');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+// const jwt = require('jsonwebtoken'); 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyi9p.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -40,7 +39,7 @@ async function run() {
       res.send(services);
     })
 
-    
+    // some chage for problem heroku server
   }
 
   finally {
